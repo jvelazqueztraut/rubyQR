47b91ac90136e9b1c8954b6ae323a94c01f7e488
Javier Velazquez Traut <javier@cran.io>
2016-09-30 16:06:45 -0300

    New main.mm based on ios configs

--- src/main.mm
+++ src/main.mm
@@ -2,11 +2,19 @@
 #include "ofApp.h"
 
 int main(){
-	ofSetupOpenGL(APP_WIDTH,APP_HEIGHT, OF_WINDOW);			// <-------- setup the GL context
-
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
+    //  here are the most commonly used iOS window settings.
+    //------------------------------------------------------
+    ofiOSWindowSettings settings;
+    settings.enableRetina = true; // enables retina resolution if the device supports it.
+    settings.enableDepth = false; // enables depth buffer for 3d drawing.
+    settings.enableAntiAliasing = true; // enables anti-aliasing which smooths out graphics on the screen.
+    settings.numOfAntiAliasingSamples = 1; // number of samples used for anti-aliasing.
+    settings.enableHardwareOrientation = false; // enables native view orientation.
+    settings.enableHardwareOrientationAnimation = false; // enables native orientation changes to be animated.
+    settings.glesVersion = OFXIOS_RENDERER_ES1; // type of renderer to use, ES1, ES2, etc.
+    
+    ofCreateWindow(settings);
+    
 	ofRunAppWithAppUtils(new ofApp());
 	return 0;
 }
