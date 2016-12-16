d770f2494f067b6e4cd23ca83c4c1bf896c2ce4c
Javier Velazquez Traut <javier@cran.io>
2016-12-16 18:10:45 -0300

    Simple version of people posting working

--- src/scenes/DatosScene.h
+++ src/scenes/DatosScene.h
@@ -5,13 +5,17 @@
 #include "ofxAnimatableObject.h"
 #include "ofxJSON.h"
 
+#include "token.h"
+
 #define DATOS_WIDTH (500*ofGetWidth()/APP_WIDTH)
 #define DATOS_HEIGHT (50*ofGetWidth()/APP_WIDTH)
+
+#define PEOPLE_POST_TIMER 0.5
 
 class DatosScene : public ofxScene {
 public:
     // set the scene name through the base class initializer
-    DatosScene(ofxSceneManager& sm, ofxJSONElement& r) : sceneManager(sm), response(r), ofxScene(DATOS_SCENE_NAME, false) {
+    DatosScene(ofxSceneManager& sm, ofxJSONElement& r, string& u, string& q) : sceneManager(sm), response(r), url(u), qr(q), ofxScene(DATOS_SCENE_NAME, false) {
         title.load("fonts/Futura/FuturaStd-Heavy.otf",36*ofGetWidth()/APP_WIDTH);
         title.setText("");
         title.setAnchorPercent(0.0,1.0);
@@ -32,6 +36,23 @@ public:
         scanButton.width=BUTTON_WIDTH;
         scanButton.height=BUTTON_HEIGHT;
         
+        peopleNumberText.load("fonts/Calibri/calibrib.ttf",24*ofGetWidth()/APP_WIDTH);
+        peopleButtonText.load("fonts/Calibri/calibri.ttf",24*ofGetWidth()/APP_WIDTH);
+        peopleTitleText.load("fonts/Calibri/calibrib.ttf",24*ofGetWidth()/APP_WIDTH);
+        peopleStatusText.load("fonts/Calibri/calibrii.ttf",24*ofGetWidth()/APP_WIDTH);
+
+        peopleButton.x=0;
+        peopleButton.y=0;
+        peopleButton.width=BUTTON_WIDTH/4;
+        peopleButton.height=BUTTON_HEIGHT;
+        peopleAddButton.x=0;
+        peopleAddButton.y=0;
+        peopleAddButton.width=BUTTON_WIDTH/6;
+        peopleAddButton.height=BUTTON_HEIGHT;
+        peopleSubButton.x=0;
+        peopleSubButton.y=0;
+        peopleSubButton.width=BUTTON_WIDTH/6;
+        peopleSubButton.height=BUTTON_HEIGHT;
     }
     
     // scene setup
@@ -61,6 +82,8 @@ public:
                 status.setText("RECHAZADO");
             }
             last.setText("Última lectura " + response["signup"]["last"].asString());
+            
+            ID = response["signup"]["ID"].asString();
         }
         else{
             title.setText("No hay datos disponibles");
@@ -76,7 +99,11 @@ public:
             
             status.setText("");
             last.setText("Por favor scanee otro código QR");
+            
+            ID = "";
         }
+        
+        MT = 1;
         
         ofPixels img;
         if(status.text=="APROBADO"){
@@ -102,6 +129,15 @@ public:
         title.setSize(0.8);
         
         scanButton.setPosition(ofGetWidth()*0.5-scanButton.width/2,ofGetHeight()-scanButton.height*1.5);
+
+        peopleButton.setPosition(ofGetWidth()*0.5+scanButton.width/2-peopleButton.width,ofGetHeight()-scanButton.height*2.75);
+        peopleSubButton.setPosition(ofGetWidth()*0.5-scanButton.width/2,ofGetHeight()-scanButton.height*2.75);
+        peopleAddButton.setPosition(ofGetWidth()*0.5-scanButton.width/2+peopleSubButton.width+BUTTON_WIDTH/3,ofGetHeight()-scanButton.height*2.75);
+        
+        peoplePostRequest=false;
+        peoplePostWaiting=false;
+        peoplePostStatus="";
+        peoplePostTimer=0.0f;
         
         time=ofGetElapsedTimef();
     }
@@ -152,7 +188,65 @@ public:
         
         status.update(dt);
         last.update(dt);
-
+        
+        if(peoplePostTimer>0.0f){
+            peoplePostTimer-=dt;
+            if(peoplePostTimer<=0.0f)
+                peoplePostTimer=0.0f;
+        }
+        
+        if(peoplePostRequest){
+            if(peoplePostWaiting){
+                // create the url string
+                string uri = url;
+                
+                uri=uri+"mode=multiple&multiple="+ofToString(MT)+"&qr="+qr+"&ID="+ID;
+                
+                if(uri.substr(0,4).compare("http")!=0)
+                    uri="http://"+uri;
+                
+                ofLogNotice(POST_SCENE_NAME) << "URI: " << uri;
+                ofHttpResponse res = ofLoadURL(uri);
+                //posted=true;
+                ofLogNotice(POST_SCENE_NAME) << "HTTP GET: " << res.status;
+                if(res.status > 0) {
+                    if(res.status == 200){
+                        bool parsing = response.parse(res.data.getText());
+                        if(parsing){
+                            string token = APP_TOKEN;
+                            if(token.compare(response["token"].asString())==0){
+                                ofLogNotice(POST_SCENE_NAME) << "Respuesta: " << res.data.getText();
+                                peoplePostStatus = response["status"].asString();
+                                //sceneManager.gotoScene(DATOS_SCENE_NAME);
+                            }
+                            else{
+                                ofLogError(POST_SCENE_NAME) << "Token invalido.";
+                                sceneManager.gotoScene(ERROR_SCENE_NAME);
+                            }
+                        }
+                        else{
+                            ofLogError(POST_SCENE_NAME) << "Problemas con el parseo de la respuesta: " << res.data.getText();
+                            sceneManager.gotoScene(ERROR_SCENE_NAME);
+                        }
+                    }
+                    else{
+                        ofLogError(POST_SCENE_NAME) << "Problemas con la URL: " << res.data.getText();
+                        sceneManager.gotoScene(ERROR_SCENE_NAME);
+                    }
+                }
+                else{
+                    ofLogError(POST_SCENE_NAME) << "Problemas con la conexi√≥n: " << res.data.getText();
+                    sceneManager.gotoScene(ERROR_SCENE_NAME);
+                }
+                peoplePostWaiting=false;
+                peoplePostRequest=false;
+                peoplePostTimer = PEOPLE_POST_TIMER;
+            }
+            else{
+                peoplePostWaiting=true;
+                peoplePostStatus = "...";
+            }
+        }
     }
     
     // called when scene is exiting
@@ -193,6 +287,38 @@ public:
         ofNoFill();
         ofDrawRectangle(scanButton);
         ofPopStyle();
+        
+        
+        ofPushStyle();
+        ofSetColor(35,200);
+        ofFill();
+        ofDrawRectangle(peopleAddButton);
+        ofDrawRectangle(peopleSubButton);
+        ofNoFill();
+        ofDrawRectangle(ofRectangle(peopleSubButton.getPosition(),peopleAddButton.getPosition()+ofPoint(peopleAddButton.width,peopleAddButton.height)));
+        ofDrawRectangle(peopleButton);
+        
+        ofSetColor(255,255);
+        peopleButtonText.drawString("+",peopleAddButton.x+peopleAddButton.width/2-peopleButtonText.stringWidth("+")/2,peopleAddButton.y+peopleAddButton.height/2+peopleButtonText.stringHeight("+")/2);
+        peopleButtonText.drawString("-",peopleSubButton.x+peopleSubButton.width/2-peopleButtonText.stringWidth("-")/2,peopleSubButton.y+peopleSubButton.height/2+peopleButtonText.stringHeight("-")/2);
+        
+        ofSetColor(35,200);
+        peopleNumberText.drawString(ofToString(MT),peopleSubButton.x+peopleSubButton.width+BUTTON_WIDTH/6-peopleNumberText.stringWidth(ofToString(MT))/2,peopleSubButton.y+peopleSubButton.height/2+peopleNumberText.stringHeight(ofToString(MT))/2);
+        
+        string peopleStr = "ENVIAR";
+        peopleButtonText.drawString(peopleStr,peopleButton.x+peopleButton.width/2-peopleButtonText.stringWidth(peopleStr)/2,peopleButton.y+peopleButton.height/2+peopleButtonText.stringHeight(peopleStr)/2);
+        
+        ofSetColor(35,255);
+        peopleStatusText.drawString(peoplePostStatus,peopleButton.x+peopleButton.width-peopleStatusText.stringWidth(peoplePostStatus),peopleButton.y-1.1*peopleStatusText.stringHeight(peoplePostStatus)/2);
+        
+        if(peoplePostRequest || peoplePostTimer > 0.0f){
+            ofFill();
+            ofSetColor(35,255);
+            ofDrawRectangle(peopleButton);
+            ofSetColor(255,255);
+            peopleButtonText.drawString(peopleStr,peopleButton.x+peopleButton.width/2-peopleButtonText.stringWidth(peopleStr)/2,peopleButton.y+peopleButton.height/2+peopleButtonText.stringHeight(peopleStr)/2);
+        }
+        ofPopStyle();
 
     }
     
@@ -206,15 +332,42 @@ public:
             return;
         if(scanButton.inside(x,y)){
             sceneManager.gotoScene(QR_SCENE_NAME);
+            return;
+        }
+        
+        if(peopleAddButton.inside(x,y)){
+            MT++;
+        }
+        
+        if(peopleSubButton.inside(x,y)){
+            if(MT>1)
+                MT--;
+        }
+        
+        if(peopleButton.inside(x,y)){
+            if(peoplePostTimer<=0.0f){
+                peoplePostRequest=true;
+            }
         }
     }
     
     float time;
     ofxJSONElement& response;
+    string& url;
+    string& qr;
+    
+    string ID;
+    unsigned int MT;
     
     ofRectangle scanButton;
     ofTrueTypeFont scanText;
     
+    ofRectangle peopleButton,peopleAddButton,peopleSubButton;
+    ofTrueTypeFont peopleNumberText,peopleButtonText,peopleTitleText,peopleStatusText;
+    bool peoplePostRequest,peoplePostWaiting;
+    string peoplePostStatus;
+    float peoplePostTimer;
+
     ofxAnimatableObject<ofTrueTypeFont> title,status,last;
     vector< ofxAnimatableObject<ofTrueTypeFont> > fields;
     
